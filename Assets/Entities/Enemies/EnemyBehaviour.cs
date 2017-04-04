267cefedd9cbec944631cb2999b52a7941993c22
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-04-03 22:23:15 -0500

    Projectile from ship can Damage and Destroy Enemy ships

---
+++ Assets/Entities/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+	public float health=150;
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Projectile missle = collider.gameObject.GetComponent<Projectile>();
+		if(missle){
+			health -=missle.GetDamage();
+			missle.Hit();
+			if(health<=0){
+				Destroy(gameObject);
+			}
+			Debug.Log ("Hit by a projectile");
+		}
+		
+	}
+}
---
+++ Assets/Entities/Projectile/Projectile.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+
+	public float damage = 100f;
+	
+	public void Hit(){
+		Destroy (gameObject);
+		GetDamage();
+	}
+	
+	public float GetDamage(){
+		return damage;
+	}
+}
