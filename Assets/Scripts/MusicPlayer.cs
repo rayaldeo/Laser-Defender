a85a4a1a981eb7e740fd089a4e1a0c3c695c912b
Jonathan Elliott <jonathan_elliott16@yahoo.com>
2017-04-18 23:22:13 -0500

    Added Separate Music For Start/Game/End Scene

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -4,26 +4,42 @@ using System.Collections;
 public class MusicPlayer : MonoBehaviour {
 
 	 static MusicPlayer instance = null;
+	 public AudioClip startClip;
+	 public AudioClip gameClip;
+	 public AudioClip endClip;
+	 
+	 private AudioSource music;
 	 
 	 
 	 void Awake(){
 	 	//Debug.Log ("Music plyer Awaked: " + GetInstanceID());
-		if(instance !=null){
+		if(instance !=null && instance != this){
 			Destroy (gameObject);
 			print ("Duplicate music player:self-destructing");
 		}else{
 			instance=this;
 			GameObject.DontDestroyOnLoad(gameObject);//This Command is used to keep music playing throughout differnt scenes
+			music = GetComponent<AudioSource>();
+			music.clip=startClip;
+			music.loop = true;
+			music.Play();
 		}
 	 }
 
-	// Use this for initialization
-	void Start () {
-		//Debug.Log ("Music plyer Start: " + GetInstanceID());
+	
+	void OnLevelWasLoaded(int level) {
+		Debug.Log ("MusicPlayer: Loaded level: "+ level);
+		music.Stop();
+		if(level==0){
+			music.clip = startClip;
+		}if(level==3){
+			music.clip = gameClip;
+		}if(level==2){
+			music.clip = endClip;
+		}
+		music.loop = true;
+		music.Play();
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
